d55e11a6f17178503c86f765b15f022fa4052e45
fersilverio <ssilveriofernando45@gmail.com>
2025-06-10 22:30:03 -0300

    Estruturando tela principal de kanban

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,65 @@
-<script setup lang="ts">
+<script setup>
+
+
+import Card from './components/Card.vue';
+import { Container, Draggable } from "vue-dndrop";
 </script>
 
 <template>
-  <div class="bg-red-500">
-    <p>aaaaa</p>
+
+  <div class="bg-gray-100 flex items-center justify-center min-h-screen">
+
+    <div class="w-full md:w-3/4 lg:w-2/3 xl:w-full bg-white p-6 rounded-lg shadow-lg flex flex-col min-h-screen">
+
+      <h1 class="text-black">sss</h1>
+      <div class="flex gap-5" style="height: 800px;">
+        <div class="flex-1 bg-blue-200 p-4 rounded-md flex flex-col  text-blue-800 font-semibold gap-2"
+          style="overflow-y: scroll;">
+
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+
+
+        </div>
+
+        <div
+          class="flex-1 bg-green-200 p-4 rounded-md flex items-center justify-center text-green-800 font-semibold h-full">
+          Coluna 2
+        </div>
+
+        <div
+          class="flex-1 bg-red-200 p-4 rounded-md flex items-center justify-center text-red-800 font-semibold h-full">
+          Coluna 3
+        </div>
+
+        <div
+          class="flex-1 bg-purple-200 p-4 rounded-md flex items-center justify-center text-red-800 font-semibold h-full">
+          Coluna 4
+        </div>
+
+        <div
+          class="flex-1 bg-green-200 p-4 rounded-md flex items-center justify-center text-red-800 font-semibold h-full">
+          Coluna 5
+        </div>
+
+
+      </div>
+
+    </div>
+
   </div>
+
+
 </template>
 
 <style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+
+<template>
+    <div class="card">
+        <div class="card-body">Hello World</div>
+    </div>
+</template>
